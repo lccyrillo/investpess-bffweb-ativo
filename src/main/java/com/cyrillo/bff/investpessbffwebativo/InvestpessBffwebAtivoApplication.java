38c756c601f360e91b6489eebb1c3d5b3c863d29
Luciano Cavallini Cyrillo <lccyrillo@gmail.com>
2021-11-13 17:09:06 -0300

    CYRILLO_FEAT_0001 - Levantando infra estrutura

---
+++ src/main/java/com/cyrillo/bff/investpessbffwebativo/InvestpessBffwebAtivoApplication.java
@@ -0,0 +1,13 @@
+package com.cyrillo.bff.investpessbffwebativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestpessBffwebAtivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvestpessBffwebAtivoApplication.class, args);
+	}
+
+}
